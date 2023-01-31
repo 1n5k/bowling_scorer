76d18cf73c4010ce7b1c8a6f73f79ba06ef3ffff
Ryutaro NAKASHIO <nakashio@haw.co.jp>
2023-02-01 00:03:53 +0900

    Add Flame class

---
+++ src/flame.ts
@@ -0,0 +1,21 @@
+type _Flame = {
+    rolls: number[];
+    isSpare(): boolean;
+    isStrike(): boolean;
+}
+
+export default class Flame implements _Flame {
+    rolls: number[];
+    
+    constructor(rolls: number[]) {
+        this.rolls = rolls;
+    }
+
+    isSpare(): boolean {
+        return this.rolls[0] !== 10 && (this.rolls[0] + this.rolls[1]) === 10;
+    }
+
+    isStrike(): boolean {
+        return this.rolls[0] === 10;
+    }
+}
---
+++ tests/flame.test.ts
@@ -0,0 +1,52 @@
+import Flame from '../src/flame'
+
+describe("Flame クラス",() => {
+    const flame = new Flame([]);
+    describe("isSpareメソッドのテスト", () => {
+        describe("正常系", () => {
+            it("2投で10本倒せなかったらスペアではない", () => {
+                flame.rolls = [4, 5];
+                expect(flame.isSpare()).toBe(false);
+            });
+    
+            it("1投目に10本倒すのはスペアではない", () => {
+                flame.rolls = [10, 0]
+                expect(flame.isSpare()).toBe(false);
+            });
+
+            it("2投で10本倒すとスペア", () => {
+                flame.rolls = [5, 5];
+                expect(flame.isSpare()).toBe(true);
+            });
+        })
+
+        describe("異常系", () => {
+            it("2投で10本以上倒すのはスペアではない", () => {
+                flame.rolls = [6, 5];
+                expect(flame.isSpare()).toBe(false);
+            });
+        });
+    });
+
+    describe("isStrikeメソッドのテスト", () => {
+        describe("正常系", () => {
+            it("1投目で10本倒さないとストライクではない", () => {
+                flame.rolls = [5, 5];
+                expect(flame.isStrike()).toBe(false);
+            });
+    
+            it("1投目で10本倒すとストライク", () => {
+                flame.rolls = [10, 0];            
+                expect(flame.isStrike()).toBe(true);
+                flame.rolls = [10];
+                expect(flame.isStrike()).toBe(true);
+            });
+        });
+
+        describe("異常系", () => {
+            it("1投目で10本以上倒すのはストライクではない", () => {
+                flame.rolls = [11, 0];
+            });
+        });
+    });
+});
